6a2bef898d60ae8a3c21da8d6bdb6794c32f8062
Shashank Devadiga <120053738+BeastxD7@users.noreply.github.com>
2024-09-09 20:00:32 +0530

    fixed realtime socker server mismatch

--- src/utils/socket.ts
+++
@@ -1,9 +0,0 @@
-// app/utils/socket.ts
-
-import { io } from 'socket.io-client';
-
-const socket = io('https://socket-server-mfkb.onrender.com/');
-
-// const socket = io('http://localhost:4000');
-
-export default socket;
