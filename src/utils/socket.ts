1b404b955293ac2df6f74c6c0ace59c37a5b0e5d
Shashank Devadiga <120053738+BeastxD7@users.noreply.github.com>
2024-09-09 19:27:03 +0530

    updated socket server

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -2,8 +2,8 @@
 
 import { io } from 'socket.io-client';
 
-// const socket = io('https://socket-server-mfkb.onrender.com/');
+const socket = io('https://socket-server-mfkb.onrender.com/');
 
-const socket = io('http://localhost:4000');
+// const socket = io('http://localhost:4000');
 
 export default socket;
